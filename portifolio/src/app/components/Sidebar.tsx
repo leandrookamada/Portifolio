34eaf63a4e7e818291bb8e3f045328ec6cfe0639
Leandrookamada <leandrolobatokmd@gmail.com>
2025-08-08 15:18:32 -0300

    primeiro commit

---
+++ portifolio/src/app/components/Sidebar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import {House, User, Cpu, FolderGit2, SendHorizontal} from "lucide-react";
+
+export default function SideBar() {
+    return (
+        <header className="flex gap-10 min-h-screen w-110 flex-col items-center justify-center p-24 bg-gray-800 shadow-xl">
+            <div className="overflow-hidden rounded-full w-[150px] h-[150px]">
+                <Image
+                    src="/image/perfil.jpeg"
+                    alt="Picture of the author"
+                    width={200}
+                    height={200}
+                    className="scale-125  transition-transform duration-300"
+                />
+            </div>
+            <nav className="flex flex-col gap-4 ">
+                <Link href="#" className="flex flex-row items-center gap-4 border-2 border-white">
+                    <House />
+                    <h1>Home</h1>
+                </Link>
+
+                <Link href="#" className="flex flex-row items-center gap-4 border-2 border-white">
+                    <User />
+                    <h1>Sobre</h1>
+                </Link>
+
+                <Link href="#" className="flex flex-row items-center gap-4 border-2 border-white">
+                    <Cpu />
+                    <h1>Tecnologias</h1>
+                </Link>
+
+                <Link href="#" className="flex flex-row items-center gap-4 border-2 border-white">
+                    <FolderGit2 />
+                    <h1>Projetos</h1>
+                </Link>
+
+                <Link href="#" className="flex flex-row items-center gap-4 border-2 border-white">
+                    <SendHorizontal />
+                    <h1>Recomendações</h1>
+                </Link>
+            </nav>
+        </header>
+    );
+}
